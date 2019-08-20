e9f600985e08bd1a9d1f65ca4f517c78d05a3f04
Alexandre Coulier <alexandre.coulier@gmail.com>
2019-08-20 16:19:14 +0200

    create the form to offer governess services

--- app/controllers/governesses_controller.rb
+++ app/controllers/governesses_controller.rb
@@ -9,10 +9,23 @@ class GovernessesController < ApplicationController
   end
 
   def new
-
+    @governess = Governess.new
+    @governess.user = current_user
   end
 
   def create
+    @governess = Governess.new(params_governess)
+    @governess.user = @user
+    if @governess.save
+      redirect_to governess_path(@governess)
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def params_governess
+    params.require(:governess).permit(:location, :description, :price, :picture)
   end
 end
