c9bb162d88ebc5a2c77e46d174df906fae4f7830
Alan Gazielly <alan@gazielly.com>
2019-08-21 15:36:02 +0200

    Dashboard: display of bookings on cards.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
   def dashboard
-
+    @bookings = Booking.where(user_id: current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :governesses, only: [:show, :new, :index, :create]
   resources :bookings, only: [:new, :create, :show]
 
+
   # verb url, to: "controller#action"
   get 'dashboard', to: 'users#dashboard'
 end
