8b1924fb13d41cd26d910c72c2ea304044458ee6
Alexandre Coulier <alexandre.coulier@gmail.com>
2019-08-21 15:24:00 +0200

    map added with markers

--- app/models/governess.rb
+++ app/models/governess.rb
@@ -1,4 +1,7 @@
 class Governess < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   belongs_to :user
   has_many :bookings
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_091439) do
+ActiveRecord::Schema.define(version: 2019_08_21_090351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_08_20_091439) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_governesses_on_user_id"
   end
 
